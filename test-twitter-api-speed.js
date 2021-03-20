8ba3236e0c4451067af97783327f5148ec496afc
German Robayo <germanrobayo33@gmail.com>
2021-03-20 11:14:31 -0400

    add scripts

---
+++ test-twitter-api-speed.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+const Twit = require('twit')
+
+const yargs = require('yargs')
+const { hideBin } = require('yargs/helpers')
+const async = require('async')
+const performance = require('perf_hooks').performance
+
+const args = yargs(hideBin(process.argv))
+    .option('numCalls', {
+        type: 'number',
+        description: 'Number of calls to statuses/show',
+        default: 11
+    })
+    .option('tweetId', {
+        type: 'number',
+        description: 'Tweet id to call Twitter API',
+        default: '1373203066556588034'
+    })
+    .boolean('parallel')
+    .demandOption('consumerKey', 'Twitter Consumer Key is required')
+    .demandOption('consumerSecret', 'Twitter Consumer Secret is required')
+    .help()
+    .argv
+
+const client = new Twit({
+    consumer_key: args.consumerKey,
+    consumer_secret: args.consumerSecret,
+    app_only_auth: true
+})
+
+const callTwitter = async (i, cb) => {
+    const startTime = performance.now()
+    const response = await client.get('statuses/show', {
+        id: args.tweetId,
+        tweet_mode: 'extended'
+    })
+    const endTime = performance.now()
+    return {
+        run: i,
+        msSpent: endTime - startTime
+    }
+
+}
+
+const gatherResults = (err, results) => {
+    if (err) throw err
+
+    console.log(results)
+}
+
+if (args.parallel) {
+    console.log('Running tests in parallel')
+    async.times(args.numCalls, callTwitter, gatherResults)
+} else {
+    console.log('Running tests in sequential')
+    async.timesSeries(args.numCalls, callTwitter, gatherResults)
+}
